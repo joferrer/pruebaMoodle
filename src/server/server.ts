b5c94c1fb59c7996b1964b47058d5e844639f3df
Jeison Ferrer <jeison120120@hotmail.com>
2025-09-09 16:53:30 -0500

    Ahora solo se permiten peticiones desde ciertos origenes

--- src/server/server.ts
+++ src/server/server.ts
@@ -18,14 +18,17 @@ class Server {
 
 
     constructor() {
-        const portEnv = variables.PORT ;
+        const portEnv = variables.PORT;
         this.port = portEnv ? parseInt(portEnv) : 3000;
         this.app = express();
 
     }
 
     middlewares() {
-        this.app.use(cors());
+        this.app.use(cors({
+            origin: ["https://evaluador-de-codigo.vercel.app", "https://simulatorchispa.netlify.app", "https://virtualpregrado.ufps.edu.co", "http://localhost:4321"],
+            credentials: true,
+        }));
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.static(path.join(__dirname, '../../public')));
